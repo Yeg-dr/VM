6ea3c3e51790890cb0b8c2b4e7fe46f6cff7b28c
Yeg <dadaryegane@gmail.com>
2025-08-13 19:21:19 +0330

    keyboard alphabet

--- panels/keyboard.py
+++ panels/keyboard.py
@@ -60,7 +60,7 @@ class Keyboard(QWidget):
         rows = [
             ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
             ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
-            ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+            ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':'],
             ['Z', 'X', 'C', 'V', 'B', 'N', 'M', '.', '-', '@'],
             ['Backspace', 'Space']  # Only these two in the second last row
         ]
